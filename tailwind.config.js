91196aa32fab6fddf282713a2339aa75ffd491eb
NajNaj <najnaj@example.com>
2025-08-05 18:11:05 +0200

    Initial commit: NajNaj site with Tailwind CSS v3 and Flask

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./static/**/*.js",
+    "./app.py"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'primary': ['Kreon', 'serif'],
+        'secondary': ['Pacifico', 'cursive'],
+      },
+      colors: {
+        'rose': '#F7B3C9',
+        'blue': '#7FCECD',
+      }
+    },
+  },
+  plugins: [],
+} 
